17f416c630810f1de2a612c1fbdc7df7ebc0f262
Roberto Samia <bojosteph@gmail.com>
2019-09-01 23:28:22 -0400

    updated namespace on controllers ans cors.rb

--- app/controllers/api/v1/user_token_controller.rb
+++ app/controllers/api/v1/user_token_controller.rb
@@ -1,3 +1,3 @@
-class UserTokenController < Knock::AuthTokenController
+class Api::V1::UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token, raise: false
 end
