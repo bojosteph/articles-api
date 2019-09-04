94b85aee718683d689d284973f316f5d30b4a42e
Roberto Samia <bojosteph@gmail.com>
2019-09-03 22:53:55 -0400

    created comment using scaffold generator with belongs to user and article

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
   scope '/api' do 
     post 'user_token' => 'user_token#create'
     resources :users
     resources :articles
+    resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
